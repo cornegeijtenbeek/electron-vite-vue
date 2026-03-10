ff05b3d83193ba64243acc2035c26d6488400786
Corné Geijtenbeek <Corne.Geijtenbeek@aebi-schmidt.com>
2026-03-10 14:36:39 +0100

    lint: add biome and format all files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,16 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+    import type { DefineComponent } from 'vue';
+    const component: DefineComponent<
+        Record<string, unknown>,
+        Record<string, unknown>,
+        unknown
+    >;
+    export default component;
 }
 
 interface Window {
-  // expose in the `electron/preload/index.ts`
-  ipcRenderer: import('electron').IpcRenderer
+    // expose in the `electron/preload/index.ts`
+    ipcRenderer: import('electron').IpcRenderer;
 }
